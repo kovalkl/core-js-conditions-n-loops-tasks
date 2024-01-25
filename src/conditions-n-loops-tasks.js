23e5c0c89dd3a1472f55a89175c75f9228a6461b
Koval Anastasiia <ilovepokyshat@gmail.com>
2024-01-25 12:54:18 +0300

    feat: add isIsoscelesTriangle func

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -90,8 +90,14 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  return (
+    a * b * c !== 0 &&
+    (a === b || a === c || b === c) &&
+    a + b > c &&
+    b + c > a &&
+    a + b > c
+  );
 }
 
 /**
