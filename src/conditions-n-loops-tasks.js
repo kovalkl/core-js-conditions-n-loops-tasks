d7ad5cc7b4a5ead24409fe69d05638ba8e67d326
Koval Anastasiia <ilovepokyshat@gmail.com>
2024-01-26 10:46:33 +0300

    feat: add isContainNumber func

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -279,8 +279,15 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const numToString = `${num}`;
+  let result = false;
+  for (let i = 0; i < numToString.length && !result; i += 1) {
+    if (+numToString[i] === digit) {
+      result = true;
+    }
+  }
+  return result;
 }
 
 /**
